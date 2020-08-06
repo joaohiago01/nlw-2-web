43ee6dd8cd2b8e58884ff7e83361bac64118d082
joaohiago01 <joaohiago96@gmail.com>
2020-08-05 21:01:58 -0300

    Day 3 finish web

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    user_id: number;
+    whatsapp: string
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.user_id
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/47699280?s=460&u=95a29f9e9b0fe118385c6923df534624be6fb749&v=4" alt="João Hiago"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>João Hiago</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Estudante do curso de Análise e Desenvolvimento de Sistemas.
-                <br/><br/>
-                Apaixonado por desafios e aprendizado.
-                No momento, embarcado no mundo JavaScript com NodeJS e React.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                    <button type="button">
+                    <a 
+                        onClick={createNewConnection} 
+                        target="_blank"
+                        href={` https://wa.me/${teacher.whatsapp}`}>
                         <img src={whatsappIcon} alt="Whatsapp" />
                         Entrar em contato
-                    </button>
+                    </a>
             </footer>
         </article>
     );
