1da84bd59b59e9aec7e61934f722284364a3405e
joaohiago01 <joaohiago96@gmail.com>
2020-08-05 22:12:56 -0300

    Add baseURL in Heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    //baseURL: 'http://localhost:3333',
+    baseURL: 'https://proffy-nlw-2.herokuapp.com',
 });
 
 export default api;
